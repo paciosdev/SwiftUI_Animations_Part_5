0679638369a5bbea5944b57ed721e941d2750571
Francesco Paciello <kekkopacio@gmail.com>
2021-04-01 16:41:25 +0200

    Initial Commit

---
+++ Flying Bird/Flying_BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flying_BirdApp.swift
+//  Flying Bird
+//
+//  Created by Kekko Paciello on 01/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Flying_BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
